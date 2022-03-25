f6b3fd381286165bc04c4e22af77daff5f06fd93
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-03-26 03:10:03 +0430

    ready

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  // parser: '@typescript-eslint/parser',
+  // parserOptions: {
+  //   project: 'tsconfig.json',
+  //   sourceType: 'module',
+  // },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+  },
+};
